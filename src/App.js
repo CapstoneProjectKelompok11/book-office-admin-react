2d456a2f7c967f4a28f9171fcbc870fd8cbb29dd
rizqihidayat3017 <rizqihidayat1234@gmail.com>
2022-07-09 01:11:24 +0700

    dashboard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import Login from "./components/Login";
+import Adminportal from "./pages/Adminportal";
 
 function App() {
   return (
-    <Login/>
+    <Adminportal/>
   );
 }
 
