0a08e5878c8fb7ad6b05f015814385551035e200
rizqihidayat3017 <rizqihidayat1234@gmail.com>
2022-07-09 14:03:39 +0700

    feat:orderDetails

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Login from "./components/Login";
+import AdminOrder from "./pages/AdminOrder";
 import Adminportal from "./pages/Adminportal";
 
 function App() {
   return (
-    <Adminportal/>
+    <AdminOrder/>
   );
 }
 
