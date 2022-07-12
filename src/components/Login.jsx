7abbce24bcf5430df03ff0eacb333b194f6f5db1
Andhika Putra Permana <92287321+andhikapp6@users.noreply.github.com>
2022-07-13 00:23:23 +0700

    login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,95 @@
 import React from "react";
 import logo from "../assets/logo.png";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../networks/api";
+
+
+
 
 const Login = () => {
+
+  const [dataForm, setDataForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isEmailValid, setEmailValid] = useState(false);
+  const [isPasswordValid, setPasswordValid] = useState(false);
+  const [isUserExist, setUserExist] = useState("");
+  const [isAllValid, setAllValid] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const [isMessage, setMessage] = useState({
+    message: "",
+  });
+
+  const handleEmail = (e) => {
+    const email = e.target.value;
+    console.log("value", email);
+
+    setEmail(email);
+    if (
+      email
+    ) {
+      setEmailValid(true);
+      setDataForm((state) => ({ ...state, email }));
+    } else {
+      setEmailValid(false);
+    }
+  };
+
+  const handlePassword = (e) => {
+    const password = e.target.value;
+    setPassword(password);
+    if (
+      password
+    ) {
+      setPasswordValid(true);
+      setDataForm((state) => ({ ...state, password }));
+    } else {
+      setPasswordValid(false);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (loading) {
+      return;
+    }
+
+    if (isEmailValid && isPasswordValid) {
+      setLoading(true);
+
+      axiosInstance
+        .post("/login", {
+          email: dataForm.email,
+          password: dataForm.password,
+        })
+        .then((response) => {
+          setUserExist("exists");
+          setAllValid("valid");
+          setLoading(false);
+          console.log("response", response)
+          navigate("/");
+          Cookies.set("token", response.data.data.token);
+        })
+        .catch((error) => {
+          setUserExist("doesn't exist");
+          setAllValid("valid");
+          setLoading(false);
+          console.log(error);
+        });
+    } else {
+      setAllValid("not valid");
+      setUserExist("");
+    }
+  };
+
   return (
     <div>
       <div className="flex w-full justify-between items-center h-[60px] px-3 py-3 text-white bg-[#4D89FF]">
