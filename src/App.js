0df7322f55d5ab17d314e080e7e2bcda80e21e3a
achmadrizky486 <achmadrizky486@gmail.com>
2022-07-18 00:24:29 +0700

    fix : page nya kebalik

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login";
-import Dashboard from "./pages/Dashboard";
 import ManageReview from "./pages/ManageReview";
 import ManagingList from "./pages/ManageList";
-import OrderDetail from "./pages/OrderDetail";
 import Inbox from "./pages/Inbox";
+import Dashboard from "./pages/Dashboard";
+import OrderDetail from "./pages/OrderDetail";
 
 function App() {
   return (
